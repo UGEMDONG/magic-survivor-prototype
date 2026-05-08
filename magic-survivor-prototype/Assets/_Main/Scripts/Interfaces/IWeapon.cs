46256bb3131971d2843c33be24d23f96abe79b96
taemin637 <a01041187914@gmail.com>
2026-05-09 02:33:48 +0900

    update: IWeapon

--- magic-survivor-prototype/Assets/_Main/Scripts/Interfaces/IWeapon.cs
+++ magic-survivor-prototype/Assets/_Main/Scripts/Interfaces/IWeapon.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public interface IWeapon
 {
     public Vector2 AttackDirection { get; }
+    public float Damage { get; }
+    public float AttackCoolTime { get; }
+    public float CurrentCoolTime { get; }
+    public bool IsAttackReady { get; }
     public void Attack();
     public void TickCoolTime(float deltaTime);
     public void SetAttackDirection(Vector2 direction);
